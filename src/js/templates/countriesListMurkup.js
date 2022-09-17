5e253bca18317522b07078136d8dbaedd0328605
AlexeyKiselyov <aleshakiselev123@gmail.com>
2022-09-17 21:11:00 +0300

    add input-countries design

--- src/js/templates/countriesListMurkup.js
+++ src/js/templates/countriesListMurkup.js
@@ -1,7 +1,7 @@
 export function countriesListMurkup(arr) {
   const countriesMurkup = arr
     .map(({ code, country }) => {
-      return `<option value=${code}>${country}</option> `;
+      return `<option value=${code} style="background-color: gray; color: #ffffff">${country}</option> `;
     })
     .join('');
   return countriesMurkup;
