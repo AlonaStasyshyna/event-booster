de30400bc211cc68ce88110e0f40ebe4c66a3756
Danil-Bochkov <106696666+Danil-Bochkov@users.noreply.github.com>
2022-09-18 21:58:16 +0300

    add pagination

--- src/js/getEvents.js
+++ src/js/getEvents.js
@@ -2,6 +2,7 @@ import { COUNTRIES_LIST } from './modules/countriesList';
 import { countriesListMurkup } from './templates/countriesListMurkup';
 import { getEvents } from './modules/getAPI';
 import { createEventCard } from './createEventCard';
+import { renderPaginationBar } from './templates/pagination';
 // --------------------------------------------
 const eventsGallery = document.querySelector('.events__cards');
 const refSearchEventsInputs = document.querySelector('.js-search-form');
@@ -16,11 +17,16 @@ refSearchFormInput.insertAdjacentHTML(
 
 getEvents('', 'US').then(data => {
   const events = data.data._embedded.events;
+  const totalPagesOfEl = data.data.page.totalPages;
+  
+  renderPaginationBar(totalPagesOfEl, currentPage);
+
   events.forEach(e => eventsGallery.innerHTML += createEventCard(e));
 });
 
 let event = '';
 let country = '';
+let currentPage = 1;
 
 function onSearchEventsInput(e) {
   eventsGallery.innerHTML = '';
@@ -35,6 +41,10 @@ function onSearchEventsInput(e) {
 
   getEvents(event, country).then(data => {
     const events = data.data._embedded.events;
+    const totalPagesOfEl = data.data.page.totalPages;
+    
+    renderPaginationBar(totalPagesOfEl, currentPage);
+
     events.forEach(e => eventsGallery.innerHTML += createEventCard(e));
   });
 };
