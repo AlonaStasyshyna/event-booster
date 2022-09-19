4acf27e3c6ac257e77411540490f49d3a66b991f
Vova Boychyshyn <93547207+qsunn@users.noreply.github.com>
2022-09-19 12:22:03 +0300

    fixed poster sizes

--- src/js/templates/createEventCard.js
+++ src/js/templates/createEventCard.js
@@ -9,7 +9,7 @@ export function createEventCard(event) {
         <li class="events__card">
             <div class="events__poster">
                 <div class="events__poster--decor"></div>
-                <img src="${event.images.find(e => e.height >= 600).url}" alt="poster" class="events__poster--img">
+                <img src="${event.images.find(e => e.height >= 400 && e.height <= 600).url}" alt="poster" class="events__poster--img">
             </div>
             <div class="events__info">
                 <h2 class="events__info--title">${event.name}</h2>
